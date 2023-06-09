d5aabfd07b79ccf9245e6e269a6f35c09a8c3f9c
Rodrigo Ceballos <rodrigo@georgian.io>
2023-06-08 20:04:59 -0400

    fix(notebooks/day-2/01.1-streamlit-example-1.py): fix error

--- notebooks/day-2/01.1-streamlit-example-1.py
+++ notebooks/day-2/01.1-streamlit-example-1.py
@@ -1,27 +1,21 @@
 import streamlit as st
 
+st.title('String List App')
 
-    st.title('String List App')
+# Try to load the list from Streamlit's session state
+# If it doesn't exist, create an empty list
+if 'string_list' not in st.session_state:
+    st.session_state.string_list = []
 
-    # Try to load the list from Streamlit's session state
-    # If it doesn't exist, create an empty list
-    if 'string_list' not in st.session_state:
-        st.session_state.string_list = []
+# Display the input field and the add button
+user_input = st.text_input('Add a string:')
+add_button = st.button('Add string')
 
-    # Display the input field and the add button
-    user_input = st.text_input('Add a string:')
-    add_button = st.button('Add string')
+# When the add button is clicked, append the string to the list
+if add_button:
+    st.session_state.string_list.append(user_input)
 
-    # When the add button is clicked, append the string to the list
-    if add_button:
-        st.session_state.string_list.append(user_input)
-        st.text_input('Add a string:', value='')  # clear the text box
-
-    # Display the current list
-    st.subheader('String List')
-    for string in st.session_state.string_list:
-        st.write(string)
-
-
-if __name__ == '__main__':
-    main()
+# Display the current list
+st.subheader('String List')
+for string in st.session_state.string_list:
+    st.write(string)
