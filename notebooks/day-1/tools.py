96378b498d020993b70fe8c90b166b8e222898ce
Akash Saravanan <akash.saravanan@georgian.io>
2023-09-28 15:19:22 -0600

    Feat(notebooks/day-1/tools.py): Add chat history options to universal llm call.

--- notebooks/day-1/tools.py
+++ notebooks/day-1/tools.py
@@ -1,6 +1,6 @@
 import openai
 from vertexai.preview.language_models import TextGenerationModel, ChatModel
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Tuple, Optional
 
 text_bison = TextGenerationModel.from_pretrained("text-bison@001")
 chat_bison = ChatModel.from_pretrained("chat-bison@001")
@@ -29,31 +29,38 @@ def llm_call(
         prompt: str, 
         parameters: Dict[str, Any]={}, 
         system_prompt: str="", 
-        chat_history: List[Dict[str, str]] = []
-    ) -> str:
+        chat_history: List=[],
+        return_chat_history=False,
+    ) -> Tuple[str, Optional[List]]:
     # Rename parameters for specific models
     parameters = parameter_handler(parameters, model)
     # Run inference based on model
     if model in ["gpt-3.5-turbo", "gpt-4"]:
         if chat_history:
-            messages = chat_history
-            messages.append({"role": "user", "content": prompt})
+            chat_history.append({"role": "user", "content": prompt})
         else:
-            messages = [
+            chat_history = [
                 {"role": "system", "content": system_prompt},
                 {"role": "user", "content": prompt}
             ]
         response = openai.ChatCompletion.create(
             model=model,
-            messages=messages,
+            messages=chat_history,
             **parameters
         )
-        return response.choices[0].message["content"]
+        result = response.choices[0].message["content"]
+        chat_history.append({"role": "assistant", "content": result})
     elif model == "chat-bison":
         chat = chat_bison.start_chat(context=system_prompt, message_history=chat_history)
         response = chat.send_message(prompt, **parameters)
-        return response.text
+        result = response.text
+        chat_history = chat.message_history
     elif model == "text-bison":
         # Text bison does not support system prompts or chat history
         response = text_bison.predict(prompt, **parameters)
-        return response.text
+        result = response.text
+        chat_history = []
+
+    if return_chat_history:
+        return result, chat_history
+    return result
