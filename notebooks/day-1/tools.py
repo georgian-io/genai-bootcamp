2a25913fb755380fc3355a3fa44f868896506257
Akash Saravanan <akash.saravanan@georgian.io>
2023-10-17 18:19:03 -0600

    Feat(.): Add Claude support & examples.

--- notebooks/day-1/tools.py
+++ notebooks/day-1/tools.py
@@ -3,25 +3,30 @@ from vertexai.preview.language_models import TextGenerationModel, ChatModel
 from typing import List, Dict, Any, Tuple, Optional
 import os
 import requests
+import boto3
+import json
 
 text_bison = TextGenerationModel.from_pretrained("text-bison@001")
 chat_bison = ChatModel.from_pretrained("chat-bison@001")
+bedrock = boto3.client(service_name='bedrock-runtime')
 
 def parameter_handler(parameters: Dict[str, Any], model: str) -> Dict[str, Any]:
     """
     This function handles the most common parameters used namely:
         temperature (no change b/w models)
         top_p (no change b/w models)
-        top_k (not present in openai; exists for google & llama)
+        top_k (not present in openai; exists for google, claude & llama)
         max_tokens (changes between models)
     """
     new_parameters = {}
     for parameter, value in parameters.items():
-        if parameter in ["max_tokens", "max_output_tokens", "n_tokens"]:
+        if parameter in ["max_tokens", "max_output_tokens", "n_tokens", "max_tokens_to_sample"]:
             if model in ["gpt-3.5-turbo", "gpt-4", "Llama-2-7b-chat-hf", "Llama-2-13b-chat-hf", "Llama-2-70b-chat-hf"]:
                 new_parameters["max_tokens"] = value
             elif model in ["chat-bison", "text-bison"]:
                 new_parameters["max_output_tokens"] = value
+            elif model in ["claude-v2", "claude-instant-v1"]:
+                new_parameters["max_tokens_to_sample"] = value
         else:
             new_parameters[parameter] = value
     return new_parameters
@@ -80,7 +85,17 @@ def llm_call(
         response = requests.request("POST", url, headers=headers, json=payload)
         result = response.json()["choices"][0]["message"]["content"]
         chat_history.append({"role": "assistant", "content": result})
-
+    elif model in ["claude-v2", "claude-instant-v1"]:
+        # Chat history and system prompts are ignored here
+        # Include everything in the prompt instead
+        model = f"anthropic.{model}"
+        payload = {
+            "prompt": prompt,
+            **parameters
+        }
+        response = bedrock.invoke_model(body=json.dumps(payload), modelId=model, accept='application/json', contentType='application/json')
+        response_body = json.loads(response.get('body').read())
+        result = response_body.get('completion')
     if return_chat_history:
         return result, chat_history
     return result
